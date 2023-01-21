2c5f443de2777358b411d957df436bcb7154e32e
ingole8888 <ingole8888@gmail.com>
2023-01-21 23:26:48 +0530

    update

--- src/Components/Photogrid.jsx
+++ src/Components/Photogrid.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Box, Grid, Image } from "@chakra-ui/react";
+import { Box, Grid, Image, Text, Link } from "@chakra-ui/react";
 import { useState } from 'react'
 import axios from "axios"
 import { useEffect } from 'react';
+import {AiOutlineTable} from "react-icons/ai"
+import {BsCamera} from "react-icons/bs"
+import {GrMultimedia} from "react-icons/gr"
 
 const Photogrid = () => {
   const [photo, setPhoto]=useState([]);
@@ -19,7 +22,24 @@ useEffect(()=>getPhoto(), [])
 //console.log(photo.length)
 
   return (
-    
+    <Box>
+      <Box margin={"auto"}
+        paddingTop={"20px"}
+        paddingBottom={"20px"}
+        width="95%"
+        display={"flex"}
+        gap={"10%"}
+        borderTop={"1px solid gray"}
+        
+        >
+        <Link marginLeft={{
+          base:"15%",
+          md:"25%",
+          lg:"35%",
+        }} display={"flex"} gap={"10%"}><AiOutlineTable fontSize={"25px"}/><Text >POSTS</Text></Link>
+        <Link display={"flex"} gap={"10%"}><GrMultimedia fontSize={"25px"}/> <Text>REELS</Text></Link>
+        <Link display={"flex"} gap={"10%"}><BsCamera fontSize={"25px"}/> <Text>TAGGED</Text></Link>
+      </Box>
     <Grid
     margin={"auto"}
     marginTop={"10px"}
@@ -51,6 +71,7 @@ useEffect(()=>getPhoto(), [])
           );
         })}
     </Grid>
+    </Box>
   )
 }
 
